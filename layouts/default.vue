9baac62f56610d55097d3f060d67a4dc62c5a0aa
MarvinVomberg <1999marvinvomberg@gmail.com>
2025-07-05 01:17:36 +0200

    add speed insights to check for any performance issues

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,12 @@
   <div class="flex min-h-full flex-col">
     <slot></slot>
   </div>
+
+  <SpeedInsights />
 </template>
 <script setup lang="ts">
 import HeaderMenu from "~/components/HeaderMenu.vue";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 
 useHead({
   title: "Farmrechner | Panthor.de",
