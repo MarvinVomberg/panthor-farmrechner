f5582a2d899a8d708241dc596f69082200ff647d
MarvinVomberg <1999marvinvomberg@gmail.com>
2025-07-04 22:17:39 +0200

    fix prices

--- pages/market.vue
+++ pages/market.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type {MarketItem, MarketItemLogData, MarketItemLogsResponse, MarketItemResponse} from "~/types/api";
 import {farmroutes} from "~/farmroutes";
+import type {GenericProduction} from "~/farmroutes/production";
 
 const allMarketItems = ref<MarketItem[] | null>(null)
 const marketHistoryData = ref<MarketItemLogData[]>([])
@@ -8,11 +9,25 @@ const loadingMarketItems = ref<boolean>(true)
 
 onMounted(() => {
   fetch('https://api.panthor.de/v1/market/1').then(_ => _.json()).then((marketResponse: MarketItemResponse) => {
-    allMarketItems.value = marketResponse.data;
+    allMarketItems.value = marketResponse.data.sort((a: MarketItem, b: MarketItem) => {
+      if (a.localized < b.localized) return -1;
+      if (a.localized > b.localized) return 1;
+      return 0;
+    });
   }).finally(() => {
     loadingMarketItems.value = false;
   })
 })
+
+const getMarketItemByName = (name: string): MarketItem | undefined => {
+  return allMarketItems.value?.find((item: MarketItem) => item.localized === name)
+}
+
+const getFarmrouteByName = (name: string|null): GenericProduction | undefined => {
+  if (!name) return undefined;
+
+  return farmroutes[name]
+}
 
 watch(allMarketItems, () => {
   if (!allMarketItems.value) return;
@@ -28,14 +43,16 @@ watch(allMarketItems, () => {
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-4 m-4">
+    <template v-if="allMarketItems?.length">
     <template v-for="(marketItemHistoryData, marketItemHistoryDataX) in marketHistoryData"
               :key="marketItemHistoryDataX">
       <MarketItemGraphCard
-          :title="farmroutes[allMarketItems[marketItemHistoryDataX]?.item]?.productLocalizedName || allMarketItems[marketItemHistoryDataX]?.item || 'Lade...'"
+          :title="getFarmrouteByName(getMarketItemByName(marketItemHistoryData[0][0].item)?.item)?.productLocalizedName || getMarketItemByName(marketItemHistoryData[0][0].item)?.localized || 'Lade...'"
           :only-graph="true"
           :height="50"
           :market-history-data="marketItemHistoryData[0]"
       />
+    </template>
     </template>
   </div>
 </template>
