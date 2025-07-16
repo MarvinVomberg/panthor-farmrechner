ee0315432959387d8c7f4e0d95760f1a30d45385
Robert Parys <kontakt@robert-parys.de>
2025-07-16 23:07:32 +0200

    ./ > ~/

--- crafting/fuel_station/index.ts
+++ crafting/fuel_station/index.ts
@@ -4,10 +4,10 @@ import {GamblingMachine} from "~/crafting/fuel_station/upgrades/GamblingMachine"
 import {Camera} from "~/crafting/fuel_station/upgrades/Camera";
 import {Lottery} from "~/crafting/fuel_station/upgrades/Lottery";
 import {AirCompressor} from "~/crafting/fuel_station/upgrades/AirCompressor";
-import {CombinationLock} from "./upgrades/CombinationLock";
-import {AttendantProtection} from "./upgrades/AttendantProtection";
-import {Payphone} from "./upgrades/Payphone";
-import {FuelStorage} from "./upgrades/FuelStorage";
+import {CombinationLock} from "~/crafting/fuel_station/upgrades/CombinationLock";
+import {AttendantProtection} from "~/crafting/fuel_station/upgrades/AttendantProtection";
+import {Payphone} from "~/crafting/fuel_station/upgrades/Payphone";
+import {FuelStorage} from "~/crafting/fuel_station/upgrades/FuelStorage";
 import {CraftCategory} from "~/types/crafting";
 import {ShoppingCartIcon} from "@heroicons/vue/20/solid";
 
